03a2df581ac77b65d88e45d23b5468ce9a7b84f7
JotaPrimo <gestald118@gmail.com>
2022-09-05 19:42:22 -0400

    add bootstrap e navbar do topo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>  
     <div>
-      <conteudo />
+      
       <topo-padrao />
+
+      <conteudo />
 
     </div>
 </template>
@@ -22,12 +24,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
