d748c49f4104ae18cdb40d51aecc6267ce813685
JotaPrimo <gestald118@gmail.com>
2022-09-07 17:00:44 -0400

    Implementando o componente PesquisarVaga

---
+++ src/components/comuns/PesquisarVaga.vue
@@ -0,0 +1,28 @@
+<template>
+
+<div class="h-100 p-5 bg-light border rounded-3">
+    <h2>Pesuisar Vagas</h2>
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label for="">Titulo do Vaga</label>
+                <input type="text" class="form-control" placeholder="Pesquisar" />
+                <small class="form-text text-muted">Informe palavras relacionadas com o titulo da vaga</small>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-primary btn-sm">Buscar</button>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'PesquisarVaga'
+}
+</script>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,72 +1,60 @@
 <template>
-  <div class="container py-4">
+<div class="container py-4">
     <div class="row">
-      <div class="h-100 p-5 bg-light border rounded-3">
-        <h2>Pesuisar Vagas</h2>
-        <div class="row">
-          <div class="col">
-            <div class="form-group">
-              <label for="">Titulo do Vaga</label>
-              <input type="text" class="form-control" placeholder="Pesquisar" />
-              <small class="form-text text-muted"
-                >Informe palavras relacionadas com o titulo da vaga</small
-              >
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col">
-            <button class="btn btn-primary btn-sm">Buscar</button>
-          </div>
-        </div>
-      </div>
+       <pesquisar-vaga />
     </div>
 
     <div class="row mt-5">
-      <div class="col-4">
-        <div class="h-100 p-5 border bg-dark text-white">
-          <p>Vagas Abertas</p>
-          <h2>25</h2>
+        <div class="col-4">
+            <div class="h-100 p-5 border bg-dark text-white">
+                <p>Vagas Abertas</p>
+                <h2>25</h2>
+            </div>
         </div>
-      </div>
 
-      <div class="col-4">
-        <div class="h-100 p-5 border bg-primary text-white">
-          <p>Proffsionais cadastrados</p>
-          <h2>125</h2>
+        <div class="col-4">
+            <div class="h-100 p-5 border bg-primary text-white">
+                <p>Proffsionais cadastrados</p>
+                <h2>125</h2>
+            </div>
         </div>
-      </div>
 
-      <div class="col-4">
-        <div class="h-100 p-5 border bg-success text-white">
-          <p>Visitantes online</p>
-          <h2>200</h2>
+        <div class="col-4">
+            <div class="h-100 p-5 border bg-success text-white">
+                <p>Visitantes online</p>
+                <h2>200</h2>
+            </div>
         </div>
-      </div>
 
     </div>
-  </div>
+</div>
 </template>
 
 <script>
+
+import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
+
 export default {
-  name: "Home",
-  created() {
-    console.log("Criado", this.teste);
-  },
-  activated() {
-    console.log("Componente é ativado");
-  },
-  deactivated() {
-    console.log("Componente é desativado");
-  },
-  beforeUnmount() {
-    console.log("Antes de demonstar/destruir");
-  },
-  unmounted() {
-    console.log("Demonstado/destruído");
-  },
+    name: "Home",
+    components: {
+      PesquisarVaga
+    },
+    
+    created() {
+        console.log("Criado", this.teste);
+    },
+    activated() {
+        console.log("Componente é ativado");
+    },
+    deactivated() {
+        console.log("Componente é desativado");
+    },
+    beforeUnmount() {
+        console.log("Antes de demonstar/destruir");
+    },
+    unmounted() {
+        console.log("Demonstado/destruído");
+    },
 };
 </script>
 
