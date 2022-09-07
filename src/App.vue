950da8d6d32cc1e8ad5c141a7f252a59df79bb82
JotaPrimo <gestald118@gmail.com>
2022-09-07 11:26:34 -0400

    ciclo de vida do compoenente: update e umnout

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
-<template>  
-    <div>
-      
-      <h1>componente APP</h1>
-      <topo-padrao />
+<template>
+  <div>
+    <h1>componente APP</h1>
+    <button @click="desmontarComponent()">
+      Desmontar o Componente Conteudo
+    </button>
+    <topo-padrao />
 
-      <conteudo />
-
-    </div>
+    <conteudo v-if="visibilidate" />
+  </div>
 </template>
 
 <script>
-import Conteudo from './components/layouts/Conteudo.vue'
-import TopoPadrao from './components/layouts/TopoPadrao.vue'
+// o @ aponta pro src, para a raiz
+import Conteudo from "@/components/layouts/Conteudo.vue";
+import TopoPadrao from "@/components/layouts/TopoPadrao.vue";
 
 // só posso usar os components depois de importar e registrar eles dentro
 // do objeto literal components
 export default {
-  name: 'App',
+  name: "App",
+  data: () => ({
+    visibilidate: true,
+  }),
+
+  methods: {
+    desmontarComponent() {
+      this.visibilidate = false;
+    },
+  },
+
   components: {
     Conteudo,
-    TopoPadrao
-  }
-}
+    TopoPadrao,
+  },
+};
 </script>
 
 <style scoped>
 /* a tag scope mantem o estilo de cada compoente dentro de si mesmo */
- /* em afetar os inferiores e superiores */
- /* sem o scop , o ultimo estilo carregado é aplicado */
-  h1 {
-    color: red;
-  }
+/* em afetar os inferiores e superiores */
+/* sem o scop , o ultimo estilo carregado é aplicado */
+h1 {
+  color: red;
+}
 </style>
--- src/components/layouts/Conteudo.vue
+++ src/components/layouts/Conteudo.vue
@@ -1,31 +1,98 @@
 <template>
   <div>
-    <h1>Componente Conteudo</h1>
-    <p>Conteudo - Propriedade $style: {{$style}}</p>
-    <div :class="$style.teste">Class teste-1</div>
-    <div :id="$style['teste-2']">ID teste-2</div>
+    <h1>{{ titulo }}</h1>
+    <button @click="atualizarComponent()">Atualizar</button>
     <home></home>
     <publicar-vaga></publicar-vaga>
   </div>
 </template>
 
 <script>
-import Home from '../views/Home.vue'
-import PublicarVaga from '../views/PublicarVaga.vue'
+import Home from "@/components/views/Home.vue";
+import PublicarVaga from "@/components/views/PublicarVaga.vue";
 export default {
-  name: 'Conteudo',
+  name: "Conteudo",
   components: {
     Home,
-    PublicarVaga
-  }
-}
+    PublicarVaga,
+  },
+
+  data: () => ({
+    teste: "O componente foi criado",
+    titulo: "Componente Conteudo",
+  }),
+
+  methods: {
+    atualizarComponent() {
+      this.titulo += "*";
+    },
+  },
+
+  beforeCreate() {
+    // não posso acessar estar propriedade antes do app ser criado
+    console.log("Antes de criar", this.teste);
+  },
+
+  created() {
+    // agora que o app foi criado, posso acessar o data
+    console.log("Criado", this.teste);
+  },
+
+  beforeMount() {
+    // posso recuperar dados por uma api e atribuir ao componente
+    // para montar o app de fato
+    console.log("Antes de Montar o template");
+  },
+  mounted() {
+    console.log("Montado");
+  },
+
+  beforeUpdate() {
+    // realcionado as atuliazações do code do componente
+    console.log("Montado");
+  },
+
+  updated() {
+    // realcionado as atuliazações do code do componente
+    console.log("Atualizado");
+  },
+
+  beforeUnmount() {
+    console.log("Antes de destruir");
+  },
+
+  unmounted() {
+    console.log("destruido");
+  },
+  /*
+  errorCaptured() {
+    console.log("Erro capturado");
+  },
+
+  renderTracked() {
+    console.log("Re-renderização rastreada");
+  },
+
+  renderTriggered() {
+    console.log("Re-renderização acionada");
+  },
+
+  activated() {
+    console.log("Componente Aativado");
+  },
+
+  deactivated() {
+    console.log("Componente desativado");
+  },
+  */
+};
 </script>
 
 <style module>
 .teste {
-  background-color: red
+  background-color: red;
 }
 #teste-2 {
-  background-color: yellow
+  background-color: yellow;
 }
 </style>
